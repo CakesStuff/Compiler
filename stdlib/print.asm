46347d142079662b2a0dbf39ebe8562bbac76276
David <no@no.no>
2023-09-04 20:09:18 +0200

    print fix (why didnt I READ)

--- stdlib/print.asm
+++ stdlib/print.asm
@@ -4,9 +4,15 @@ global print
 print:
     mov rax, 1
     mov rdi, 1
+    mov rdx, 1
     pop rbx
     pop rsi
     push rbx
-    mov rdx, 13
+.loop:
+    cmp byte [rsi], 0
+    je .end
     syscall
+    inc rsi
+    jmp .loop
+.end:
     ret
